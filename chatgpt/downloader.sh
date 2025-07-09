3bd5e46f5fef0c0c825cc360fd6122f514a8446e
you-oops-dev[silent] <you-oops-dev[silent]@users.noreply.github.com>
2025-07-09 06:01:17 +0300

    Edit source ChatGPT

--- chatgpt/downloader.sh
+++ chatgpt/downloader.sh
@@ -19,6 +19,50 @@ download_domain() {
 download_ip "${NAME_SERVICE}" > "${HOME_GITHUB}"/"${NAME_SERVICE}"/ipv4.txt
 #download_domain "${NAME_SERVICE}" > "${HOME_GITHUB}"/"${NAME_SERVICE}"/domain.txt
 
+curl --max-time 30 --retry-delay 3 --retry 10 -4s -# https://raw.githubusercontent.com/antonme/ipnames/master/dns-openai.txt >> "${HOME_GITHUB}"/"${NAME_SERVICE}"/domain.txt
+curl --max-time 30 --retry-delay 3 --retry 10 -4s -# https://raw.githubusercontent.com/antonme/ipnames/master/ext-dns-openai.txt >> "${HOME_GITHUB}"/"${NAME_SERVICE}"/domain.txt
+echo 'ab.chatgpt.com
+api.openai.com
+arena.openai.com
+auth.openai.com
+auth0.openai.com
+beta.api.openai.com
+beta.openai.com
+blog.openai.com
+cdn.oaistatic.com
+cdn.openai.com
+community.openai.com
+contest.openai.com
+debate-game.openai.com
+discuss.openai.com
+files.oaiusercontent.com
+gpt3-openai.com
+gym.openai.com
+help.openai.com
+ios.chat.openai.com
+jukebox.openai.com
+labs.openai.com
+microscope.openai.com
+oaistatic.com
+openai.com
+openai.fund
+openai.org
+platform.api.openai.com
+platform.openai.com
+spinningup.openai
+chat.openai.com
+chatgpt.com
+featureassets.org
+cdnjs.cloudflare.com
+cdn.auth0.com
+prodregistryv2.org' >> "${HOME_GITHUB}"/"${NAME_SERVICE}"/domain.txt
+dos2unix "${HOME_GITHUB}"/"${NAME_SERVICE}"/domain.txt
+sort "${HOME_GITHUB}"/"${NAME_SERVICE}"/domain.txt | uniq | sponge "${HOME_GITHUB}"/"${NAME_SERVICE}"/domain.txt
+# Prepare domain
+# Delete subdomain in file
+cat "${HOME_GITHUB}"/"${NAME_SERVICE}"/domain.txt | grep -vEe '(.openai.com|.openai.org|.openai.com.cdn.cloudflare.net|.oaistatic.com|.chatgpt.com)$' | sed '/.ua/d' | sponge "${HOME_GITHUB}"/"${NAME_SERVICE}"/domain.txt
+sort -h "${HOME_GITHUB}"/"${NAME_SERVICE}"/domain.txt | uniq | sponge "${HOME_GITHUB}"/"${NAME_SERVICE}"/domain.txt
+
 if [[ -f "${HOME_GITHUB}/${NAME_SERVICE}/ipv4.txt" ]] && [[ -f "${HOME_GITHUB}/${NAME_SERVICE}/domain.txt" ]] && [[ -f "${HOME_GITHUB}/${NAME_SERVICE}/domain_wildcard.txt" ]]; then
     jq -n \
         --slurpfile domain_data <(jq -R . "${HOME_GITHUB}/${NAME_SERVICE}/domain.txt") \
